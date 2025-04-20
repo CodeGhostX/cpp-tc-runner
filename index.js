72eae0aa075d057845ed0401da2eba957eb50775
Abhishek Kumar <abhitechy17@gmail.com>
2025-04-20 14:56:53 +0530

    Usable version is ready

--- index.js
+++ index.js
@@ -35,6 +35,6 @@ app.post("/run", (req, res) => {
   });
 });
 
-app.listen(PORT, ()=>{
+app.listen(PORT, "0.0.0.0", ()=>{
   console.log(`Server is running on port ${PORT}`);
 })
---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define mod 1000000007
+#define int long long
+
+void find(){
+  cout<<"hello World";
+}
+
+signed main(){
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+  int t = 1;
+  // cin >> t;
+  while (t--) find();
+  return 0;
+}
